aa5fd1a59245f005d58d1193a5a43e64f473e776
Dyras <johan.forsberg@my.com>
2022-09-19 22:32:50 +0200

    Implementerar Grading och Course_Feedback i själva programmet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,27 @@ public class Main {
     String token = authenticate.TokenCreator(usernameInput, passwordInput);
     boolean result = authenticate.UserValidation(usernameInput, passwordInput, token);
         if (result){
+            final String CLAIMS_ROLE = authenticate.roleGet(token);
             System.out.println("Grattis! Inloggningen lyckades!");
+            System.out.println("Du har användarrättigheten: " + CLAIMS_ROLE);
+            System.out.println("Vilken domän vill du veta dina behörigheter på?");
+            System.out.println("Alternativ: Grading eller Course_feedback");
+            String domain;
+            try {
+                domain = new Scanner(System.in).nextLine().toUpperCase();
+                if (!domain.equals("GRADING") && !domain.equals("COURSE_FEEDBACK")){
+                    System.out.println("Felaktig input! Väljer Grading som standardsvar");
+                    domain = "GRADING";
+                }
+            } catch(Exception e){
+                System.out.println("Felaktig input! Väljer Grading som standardsvar");
+                domain = "GRADING";
+            }
+            String[] list;
+            list = authenticate.roleControl(token, domain);
+            System.out.println("Dina behörigheter inom: " + list[0]);
+            System.out.println(list[1]);
+
         } else
             System.out.println("Beklagar! Fel användarnamn eller lösenord!");
     }
