b366535c05b5b7de12da0d9ce92d5f6296207961
Dyras <johan.forsberg@my.com>
2022-09-19 11:06:47 +0200

    Gör klart testet för VG-uppgiften

--- src/main/java/Authenticate.java
+++ src/main/java/Authenticate.java
@@ -1,16 +1,17 @@
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.*;
 
 public class Authenticate {
-    final List<User> userList = Arrays.asList(new User("anna", "losen"), new User("berit", "123456"), new User("kalle", "password"));
-    final private String randomText = (UUID.randomUUID().toString());
-    final private Key key = Keys.hmacShaKeyFor(randomText.getBytes());
+    final List<User> USER_LIST = Arrays.asList(new User("anna", "losen"), new User("berit", "123456"), new User("kalle", "password"));
+    final private String RANDOM_TEXT = (UUID.randomUUID().toString());
+    final private Key KEY = Keys.hmacShaKeyFor(RANDOM_TEXT.getBytes());
 
     public boolean UserValidation(String username, String password, String token ) {
         String validOrNot = Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(KEY)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
@@ -27,13 +28,22 @@ public class Authenticate {
     }
 
     public String TokenCreator(String username, String password) {
+        String role = "placeholder";
+        if (Objects.equals(username, "anna")){
+            role = "ADMIN";
+        } else if (Objects.equals(username, "berit")){
+            role = "TEACHER";
+        }else {
+                role = "STUDENT";
+        }
 
-            for (User user : userList) {
+            for (User user : USER_LIST) {
                 if (username.equals(user.username) && password.equals(user.password)) {
                     String token = Jwts.builder()
                             .setSubject(username)
                             .addClaims(Map.of(user.password, user.username))
-                            .signWith(key)
+                            .claim("ROLE", role)
+                            .signWith(KEY)
                             .compact();
 
                     return token;
@@ -43,4 +53,10 @@ public class Authenticate {
             // Det här resulterar i en exception då denna token har för få kommatecken för en riktig token
             return "falsetoken";
         }
-        }
+
+    public String roleGet(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
+        String role = claims.get("ROLE", String.class);
+        return role;
+    }
+}
--- src/test/java/AuthenticateTest.java
+++ src/test/java/AuthenticateTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -22,5 +23,20 @@ public class AuthenticateTest {
         //Then
         assertEquals(expected, result);
 
+    }
+
+    @Test
+    public void tokenTest(){
+        //Given
+        String a = "anna";
+        String b = "losen";
+        String expected = "ADMIN";
+
+        //When
+        String returnedToken = authenticate.TokenCreator(a, b);
+        String result = authenticate.roleGet(returnedToken);
+        //Then
+
+        assertEquals(expected, result);
     }
 }
