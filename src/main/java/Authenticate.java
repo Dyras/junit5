7cccbe15ad791bd1cfe2547f3ee1c8185912faf1
Dyras <johan.forsberg@my.com>
2022-09-19 11:45:35 +0200

    Lägger till ett test för att kontrollera vilken lista som returnerats. Lite överflödigt IMO.

--- src/main/java/Authenticate.java
+++ src/main/java/Authenticate.java
@@ -9,54 +9,78 @@ public class Authenticate {
     final private String RANDOM_TEXT = (UUID.randomUUID().toString());
     final private Key KEY = Keys.hmacShaKeyFor(RANDOM_TEXT.getBytes());
 
-    public boolean UserValidation(String username, String password, String token ) {
+    public boolean UserValidation(String username, String password, String token) {
         String validOrNot = Jwts.parserBuilder()
                 .setSigningKey(KEY)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .get(password, String.class);
-        if (Objects.equals(validOrNot, username))
-        {
+        if (Objects.equals(validOrNot, username)) {
             System.out.println("Din token är: " + token);
             System.out.println("Token validerad. Din token var Giltig!");
             return true;
-        }
-        else
+        } else
             System.out.println("Token kunde INTE valideras. Fel token!");
         return false;
     }
 
     public String TokenCreator(String username, String password) {
         String role = "placeholder";
-        if (Objects.equals(username, "anna")){
+        if (Objects.equals(username, "anna")) {
             role = "ADMIN";
-        } else if (Objects.equals(username, "berit")){
+        } else if (Objects.equals(username, "berit")) {
             role = "TEACHER";
-        }else {
-                role = "STUDENT";
+        } else {
+            role = "STUDENT";
         }
 
-            for (User user : USER_LIST) {
-                if (username.equals(user.username) && password.equals(user.password)) {
-                    String token = Jwts.builder()
-                            .setSubject(username)
-                            .addClaims(Map.of(user.password, user.username))
-                            .claim("ROLE", role)
-                            .signWith(KEY)
-                            .compact();
+        for (User user : USER_LIST) {
+            if (username.equals(user.username) && password.equals(user.password)) {
+                String token = Jwts.builder()
+                        .setSubject(username)
+                        .addClaims(Map.of(user.password, user.username))
+                        .claim("ROLE", role)
+                        .signWith(KEY)
+                        .compact();
 
-                    return token;
+                return token;
 
-                }
             }
-            // Det här resulterar i en exception då denna token har för få kommatecken för en riktig token
-            return "falsetoken";
         }
+        // Det här resulterar i en exception då denna token har för få kommatecken för en riktig token
+        return "falsetoken";
+    }
 
     public String roleGet(String token) {
         Claims claims = Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
         String role = claims.get("ROLE", String.class);
         return role;
+    }
+
+    public String[] roleControl(String token, String domain) {
+        String role = roleGet(token);
+        if (Objects.equals(role, "ADMIN")) {
+            if (Objects.equals(domain, "GRADING")) {
+                return new String[]{"GRADING", "READ, WRITE"};
+            } else{
+                return new String[]{"COURSE_FEEDBACK", "READ, WRITE"};
+            }
+        } else if (Objects.equals(role, "STUDENT")) {
+            if (Objects.equals(domain, "GRADING")) {
+                return new String[]{"GRADING", "READ"};
+            } else{
+                return new String[]{"COURSE_FEEDBACK", "READ, WRITE"};
+            }
+
+            }
+        else if (Objects.equals(role, "TEACHER")){
+            if (Objects.equals(domain, "GRADING")){
+                return new String[]{"GRADING", "READ, WRITE"};
+            } else{
+                return new String[]{"COURSE_FEEDBACK", "WRITE"};
+            }
+        }
+    return null;
     }
 }
--- src/test/java/AuthenticateTest.java
+++ src/test/java/AuthenticateTest.java
@@ -10,7 +10,7 @@ public class AuthenticateTest {
     private final Authenticate authenticate = new Authenticate();
 
     @ParameterizedTest
-    @CsvSource(value = {"anna, losen, true" , "berit, 123456, true", "kalle, passwor, true" } )
+    @CsvSource(value = {"anna, losen, true" , "berit, 123456, true", "kalle, password, true" } )
     public void authenticateTest(String a, String b, boolean expected){
         //Given
 
@@ -38,5 +38,23 @@ public class AuthenticateTest {
         //Then
 
         assertEquals(expected, result);
+    }
+    @Test
+    public void tokenControlTest(){
+        //GIVEN
+        String a = "anna";
+        String b = "losen";
+        String c = "GRADING";
+        String expected = "READ, WRITE";
+
+        //When
+        String returnedToken = authenticate.TokenCreator(a, b);
+        String[] roleResult = authenticate.roleControl(returnedToken, c);
+        String result = roleResult[1];
+
+        //Then
+
+        assertEquals(expected, result );
+
     }
 }
